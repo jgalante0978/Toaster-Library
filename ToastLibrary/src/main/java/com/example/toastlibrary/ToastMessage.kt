77641add2c3e8593c8580f585107f8d0a5e6ddde
Joseph Galante <joegalante714@gmail.com>
2024-03-27 22:14:31 -0500

    Update ToastMessage.kt

--- ToastLibrary/src/main/java/com/example/toastlibrary/ToastMessage.kt
+++ ToastLibrary/src/main/java/com/example/toastlibrary/ToastMessage.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 class ToastMessage {
     companion object {
         fun s(c: Context, message: String) {
-            Toast.makeText(c, message, Toast.LENGTH_SHORT)
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
         }
     }
 }
